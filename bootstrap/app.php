b84e1da925e32f4073ee014a2eb8d5af8948377a
simpledevweb <alpamisibraymov4091@gmail.com>
2024-12-05 21:40:16 +0500

    feat: install api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,10 +3,12 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -14,5 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (HttpException $e) {
+            return response()->json([
+                'status' => $e->getStatusCode(),
+                'message' => $e->getMessage()
+            ]);
+        });
     })->create();
