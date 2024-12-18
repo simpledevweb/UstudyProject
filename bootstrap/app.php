e67246be83ec23a52ac4e46c16673b48d5fd821d
Alpamis Ibraymov <ibraymovalpamis81@gmail.com>
2024-12-18 13:54:25 +0500

    feat: added middleware ApiJson & configured exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Auth\AuthenticationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +13,37 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->group('api', middleware: [
+            \App\Http\Middleware\ApiJson::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->render(function (AuthenticationException $e) {
+            return response([
+                'status' => 401,
+                'message' => $e->getMessage(),
+            ], 401);
+        });
+
+        $exceptions->render(function (AuthenticationException $e) {
+            return response([
+                'status' => 403,
+                'message' => $e->getMessage(),
+            ], 403);
+        });
+
+        $exceptions->render(function (HttpException $e) {
+            return response()->json([
+                'status' => $e->getStatusCode(),
+                'message' => $e->getMessage()
+            ], $ex->getStatusCode());
+        });
+
+        $exceptions->render(function (\Throwable $e) {
+            return response([
+                'status' => 500,
+                'message' => $e->getMessage(),
+            ], 500);
+        });
     })->create();
+    
