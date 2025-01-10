29b253c43bb2e6fc7d1655381ceed7b002b561b7
Alpamis Ibraymov <ibraymovalpamis81@gmail.com>
2025-01-10 12:00:14 +0500

    feat: added Softdelete to Posts table

--- database/migrations/2024_12_06_162535_create_posts_table.php
+++ database/migrations/2024_12_06_162535_create_posts_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->bigInteger('shared')->default(0);
             $table->boolean('recommended')->default(false);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
