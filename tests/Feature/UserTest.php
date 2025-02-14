eb6e6c62c6656272c244b7a8ec1ee5b68c01e7e3
Alpamis Ibraymov <ibraymovalpamis81@gmail.com>
2025-02-14 14:26:40 +0500

    test: created Feature test for User login

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Country;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $seed = true;
+    /**
+     * A basic feature test user loing.
+     */
+    public function test_user_login(): void
+    {
+        $user = User::create([
+            'country_id' => Country::inRandomOrder()->first()->id,
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'email_verified_at' => now(),
+            'phone' => '123456789123',
+            'phone_verified_at' => now(),
+            'password' => "12345678"
+        ]);
+
+        $response = $this->postJson('/api/core/v1/auth/login', [
+            'phone' => 123456789123,
+            'password' => "12345678"
+        ]);
+
+        $response
+        ->assertStatus(200)
+        ->assertJsonStructure([
+            'status',
+            'message',
+            'data' => [
+                'access_token',
+                'refresh_token',
+                'at_expired_at',
+                'rf_expired_at'
+            ]
+        ]);
+
+        $this->assertAuthenticatedAs($user);
+    }
+}
